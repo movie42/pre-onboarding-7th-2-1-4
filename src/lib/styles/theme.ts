5647980046746b135e0666b806817b857bf3c79d
A <movie3342@gmail.com>
2022-11-02 15:50:48 +0900

    project 세팅

---
+++ src/lib/styles/theme.ts
@@ -0,0 +1,17 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  color: {
+    main: "#0094FF",
+    mainHover: "#1731FF",
+    error: "#FA2219",
+    success: "#4BEF3B",
+    warning: "#FFD500",
+    gray100: "#D9D9D9",
+    gray200: "#434343",
+    fontBlack: "#000000",
+    fontWhite: "#fff",
+    primary: "#6a5acd",
+    red: "#c93434"
+  }
+};
