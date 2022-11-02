13008a7af4cd83eb27c06d5e67ab35d5a2fbdfb3
movie42 <movie3342@gmail.com>
2022-11-02 21:35:13 +0900

    feat:Router MainLayout 적용

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -1,10 +1,11 @@
+import MainLayouts from "@/Components/Layouts/MainLayouts";
 import { Details, Main } from "@/Pages";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 const Router = () => {
   return (
     <Routes>
-      <Route path="/">
+      <Route path="/" element={<MainLayouts />}>
         <Route index element={<Main />} />
         <Route path="/:carId" element={<Details />} />
       </Route>
